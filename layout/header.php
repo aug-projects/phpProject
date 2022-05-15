fa17dbc54746a0902bcd627267c52dae72f0edb6
Haytham Salama <haythamasalama@gmail.com>
2022-05-16 01:45:37 +0200

    Update header.php

--- layout/header.php
+++ layout/header.php
@@ -1,3 +1,20 @@
-<header>
-ssssssss
+<header class="flex justify-between bg-primary p-2">
+    <!--  menu  -->
+    <div class="flex">
+        <?php
+            foreach ($headerMenus as $menu) {
+                echo "
+                    <ul>
+                        <li>
+                            <a href='{$menu['url']}' class='text-black font-navbar' >{$menu['title']}</a>
+                        </li>
+                    </ul>
+                ";
+            }
+        ?>
+    </div>
+    <!--  logo  -->
+    <div>
+        <img src="/assets/images/logo.png" alt="logo" width="60px" title="logo"/>
+    </div>
 </header>
