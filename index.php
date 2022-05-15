fffa38c641229e07e6a9d61ac4c75d7d3a4b1bc4
Haytham Salama <haythamasalama@gmail.com>
2022-05-16 01:45:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Project php</title>
-    <link rel="stylesheet" href="assets/css/main.css">
-</head>
-<body>
 <?php
     include "ini.php";
-    include $footer;
-    include $pages['info']['dir'];
-?>
-</body>
-</html>
+    include $layout['home'];
